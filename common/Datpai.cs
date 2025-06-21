0a4c0ea8282eaa2077dbac35951c58b86daa85c3
resutto <resutto@izycon.com.br>
2025-06-20 22:52:47 -0400

    Add files via upload

---
+++ common/Datpai.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Text;
+using Dapper;
+using egourmetAPI.Model;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace IzyLav.common
+{
+    public static class Datpai
+    {
+        public static string RemoveAccents(this string text)
+        {
+            StringBuilder sbReturn = new StringBuilder();
+            var arrayText = text.Normalize(NormalizationForm.FormD).ToCharArray();
+            foreach (char letter in arrayText)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(letter) != UnicodeCategory.NonSpacingMark)
+                    sbReturn.Append(letter);
+            }
+            
+            return sbReturn.ToString().ToUpper();
+        }
+
+        public static IdLanc GerarIdLanc(int empCodigo, FbConnection con1, string query)
+        {
+            try
+            {
+                IdLanc id;
+                id = con1.Query<IdLanc>(query, new { empresa = empCodigo }).FirstOrDefault();
+                if (id.idLanc == 0)
+                {
+                    id.idLanc = 1;
+                }
+                return id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+
+    }
+}
